3cd1cb7161f4454a526797d0926111211f48e06b
Loren <hanxj@kinco.cn>
2024-07-20 15:59:15 +0800

    完成基本功能

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , m_serialCOM(new MySerialPort(this))
+    , serialTypeC(new MySerialPort(this))
+    , m_timer(new QTimer(this))
 {
     ui->setupUi(this);
+
+    connect(m_serialCOM, &QSerialPort::readyRead, this, &MainWindow::readDataFromCOM);
+    connect(serialTypeC, &QSerialPort::readyRead, this, &MainWindow::readDataFromTypeC);
+    connect(m_timer, &QTimer::timeout, this, &MainWindow::checkSerialPorts);
 }
 
 MainWindow::~MainWindow()
@@ -18,10 +26,91 @@ MainWindow::~MainWindow()
 ///
 void MainWindow::on_pushButtonRun_clicked()
 {
-    if (ui->pushButtonRun->text() == "Run") {
-        ui->pushButtonRun->setText("Stop");
+    if (ui->pushButtonRun->text() == tr("Run")) {
+        m_serialCOM->setPortName(ui->comboBoxCOM->currentText());
+        serialTypeC->setPortName(ui->comboBoxTypeC->currentText());
+
+        if (!m_serialCOM->open(QIODevice::ReadWrite)) {
+            qDebug() << tr("Failed to open COM port");
+            return;
+        }
+
+        if (!serialTypeC->open(QIODevice::ReadWrite)) {
+            qDebug() << tr("Failed to open Type-C port");
+            m_serialCOM->close();
+            return;
+        }
+
+        m_timer->start(1000);  // 每秒检查一次串口状态
+        ui->pushButtonRun->setText(tr("Stop"));
     } else {
-        ui->pushButtonRun->setText("Run");
+        m_serialCOM->close();
+        serialTypeC->close();
+        m_timer->stop();
+        ui->pushButtonRun->setText(tr("Run"));
     }
 }
 
+///
+/// \brief MainWindow::readDataFromCOM
+///
+void MainWindow::readDataFromCOM()
+{
+    QByteArray data = m_serialCOM->readAll();
+    serialTypeC->write(data);
+}
+
+///
+/// \brief MainWindow::readDataFromTypeC
+///
+void MainWindow::readDataFromTypeC()
+{
+    QByteArray data = serialTypeC->readAll();
+    m_serialCOM->write(data);
+}
+
+///
+/// \brief MainWindow::on_pushButtonRefresh_clicked
+///
+void MainWindow::on_pushButtonRefresh_clicked()
+{
+    ui->comboBoxCOM->clear();
+    ui->comboBoxTypeC->clear();
+
+    foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts()) {
+        ui->comboBoxCOM->addItem(info.portName());
+        ui->comboBoxTypeC->addItem(info.portName());
+    }
+}
+
+///
+/// \brief MainWindow::showEvent
+/// \param event
+///
+void MainWindow::showEvent(QShowEvent *event)
+{
+    QMainWindow::showEvent(event);
+    on_pushButtonRefresh_clicked();
+}
+
+///
+/// \brief MainWindow::checkSerialPorts
+///
+void MainWindow::checkSerialPorts()
+{
+    if (ui->pushButtonRun->text() == tr("Stop")) {
+        QStringList comPorts = MySerialPort::availablePorts();
+        QString comPort = ui->comboBoxCOM->currentText();
+        QString typeCPort = ui->comboBoxTypeC->currentText();
+
+        if (!comPorts.contains(comPort)) {
+            QMessageBox::warning(this, tr("Warning"), tr("COM port is disconnected"));
+            on_pushButtonRun_clicked();
+        }
+
+        if (!comPorts.contains(typeCPort)) {
+            QMessageBox::warning(this, tr("Warning"), tr("Type-C port is disconnected"));
+            on_pushButtonRun_clicked();
+        }
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QTimer>
+#include <QMessageBox>
+
+#include "myserialport.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,8 +25,19 @@ public:
 
 private slots:
     void on_pushButtonRun_clicked();
+    void on_pushButtonRefresh_clicked();
+    void readDataFromCOM();
+    void readDataFromTypeC();
+    void checkSerialPorts();
 
 private:
     Ui::MainWindow *ui;
+    MySerialPort *m_serialCOM;  // 测试设备连接的串口
+    MySerialPort *serialTypeC;  // 驱动器连接的串口
+    QTimer *m_timer;           // 定时器
+
+protected:
+    void showEvent(QShowEvent *event);
 };
+
 #endif // MAINWINDOW_H
